1945a41e7868c2e613876c12dd411ad30a556d10
srjm23 <marcelo15beserra@gmail.com>
2026-07-03 13:21:36 -0300

    test

--- backend.tf
+++ backend.tf
@@ -1,13 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = var.name_s3_backend
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = var.name_s3_backend
-    encrypt        = true
-  }
-}
-
-terraform {
   backend "s3" {}
 }
