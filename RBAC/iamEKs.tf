8a7e78660ec5c9ff3da97289a7836a65c51702b8
srjm23 <marcelo15beserra@gmail.com>
2026-08-10 15:23:59 -0300

    Feat: RBAC

---
+++ RBAC/iamEKs.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_user" "developer" {
+  name = "jm-dev"
+}
+
+resource "aws_eks_access_entry" "developer" {
+  cluster_name      = aws_eks_cluster.srjm-eks.name
+  principal_arn     = aws_iam_user.developer.arn
+  kubernetes_groups = ["developers"]
+}
