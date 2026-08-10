40a64433f7c03f4a4c1cec870dcd468f2049ae6a
srjm23 <marcelo15beserra@gmail.com>
2026-08-10 16:24:22 -0300

    changes-del

--- iamEKs.tf
+++ iamEKs.tf
@@ -1,30 +1,30 @@
-resource "aws_iam_user" "developer" {
-  name = "jm-dev"
-}
+# resource "aws_iam_user" "developer" {
+#   name = "jm-dev"
+# }
 
-resource "aws_eks_access_entry" "developer" {
-  cluster_name      = aws_eks_cluster.srjm-eks.name
-  principal_arn     = aws_iam_user.developer.arn
-  kubernetes_groups = ["developers"]
-}
+# resource "aws_eks_access_entry" "developer" {
+#   cluster_name      = aws_eks_cluster.srjm-eks.name
+#   principal_arn     = aws_iam_user.developer.arn
+#   kubernetes_groups = ["developers"]
+# }
 
-resource "aws_iam_user_policy" "jm_dev_eks" {
-  name = "jm-dev-eks-access"
-  user = aws_iam_user.developer.name
+# resource "aws_iam_user_policy" "jm_dev_eks" {
+#   name = "jm-dev-eks-access"
+#   user = aws_iam_user.developer.name
 
-  policy = jsonencode({
-    Version = "2012-10-17"
+#   policy = jsonencode({
+#     Version = "2012-10-17"
 
-    Statement = [
-      {
-        Effect = "Allow"
+#     Statement = [
+#       {
+#         Effect = "Allow"
 
-        Action = [
-          "eks:DescribeCluster"
-        ]
+#         Action = [
+#           "eks:DescribeCluster"
+#         ]
 
-        Resource = "arn:aws:eks:us-east-2:166611709615:cluster/srjm-eks"
-      }
-    ]
-  })
-}
+#         Resource = "arn:aws:eks:us-east-2:166611709615:cluster/srjm-eks"
+#       }
+#     ]
+#   })
+# }
